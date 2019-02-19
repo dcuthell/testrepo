d32cba7915aae01f537b05a6511a38659082c15c
David Cuthell <dcc1447@gmail.com>
2019-02-18 20:24:19 -0800

    change request to POST

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.min.css'
 
 class App extends Component {
+
+
   goSup(){
-    fetch('http://www.grailsoft.com/sup.php')
+    fetch('http://www.grailsoft.com/sup.php',  {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        body: JSON.stringify({access_code : '6363559180.e98b264.d1763cf8104c4840ba64d9226c2bc70c'}), // body data type must match "Content-Type" header
+    })
     .then((res) => {
       // console.log(res)
       return res.json();
